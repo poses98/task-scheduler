9c48c0647d3f77970fee72a3237592c8ea83a108
poses98 <pablo98oses@gmail.com>
2023-11-11 17:35:52 +0100

    Modified main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,2 @@
-from myPackage.util import ceil_sqr
 
-
-def print_ceil_sqr():
-    x = float(input('Insert number:'))
-    print(f'Number:{x}, whose ceil squared is {ceil_sqr(x)} ')
-
-
-if __name__ == '__main__':
-    print_ceil_sqr()
+print('Task Scheduler')
