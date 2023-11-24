b2f1c3b5c1fadbba798eb3dfab54099796f6616b
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-11-24 15:58:29 +0100

    testResources implemented in checkings. Comprobation of its correct use un testing_checkings. Testing checkResources in main

--- src/checks/checkings.py
+++ src/checks/checkings.py
@@ -2,10 +2,30 @@ def checkDependencies(path, task_dependencies):
    """
    Returns true if all dependencies are fulfilled, false otherwise.
    Parameters:
-   - path: List of task indices representing the partial path.
-   - task_dependencies: List of tuples representing task dependencies.
+   - path: list of task indices representing the partial path
+   - task_dependencies: list of tuples representing task dependencies
    """
    for task, dependent_task in task_dependencies:
        if dependent_task in path and task not in path:
            return False
    return True
+
+def checkResources(path, task_duration, task_resource, resources):
+    """
+    Returns true if the number of used resources for a partial path does
+    not exceed R. False otherwise
+    :param path: list of task indices representing the partial path
+    :param task_duration: list of task durations
+    :param task_resource: list of task resource requirements
+    :param resources: total number of available resources (R)
+    :return:
+    """
+    used_resources = [0] * resources
+
+    for task in path:
+        task_index = task - 1
+        used_resources[task_resource[task_index] - 1] += task_duration[task_index]
+         #Check if exceeds the total resources
+        if used_resources[task_resource[task_index] - 1] > resources:
+            return False
+    return True
--- src/checks/testing_checkings.py
+++ src/checks/testing_checkings.py
@@ -1,5 +1,9 @@
 from src.checks.checkings import checkDependencies
 from src.upmproblems.rcpsp06 import get_task_dependencies as rcpsp06_get_task_dependencies
+
+from src.checks.checkings import checkResources
+from src.upmproblems.rcpsp06 import get_task_duration, get_task_resource
+
 """
 DEPENDENCIES
 ---------------------------------------------------------------------------------------------------
@@ -7,23 +11,51 @@ DEPENDENCIES
 # Test case 1: Partial path with fulfilled dependencies for rcpsp06
 def testDependenciesFullfilled():
    partial_path_1 = [1, 2, 3, 5]
-   if checkDependencies(partial_path_1, rcpsp06_get_task_dependencies()):
-       print("Test case 1: Dependencies fulfilled for rcpsp06")
-   else:
-       print("Test case 1: Dependencies not fulfilled for rcpsp06")
+   result = checkDependencies(partial_path_1, rcpsp06_get_task_dependencies())
+   resultDependencies(result)
    return 0
-
 
 # Test case 2: Partial path with unfulfilled dependencies for rcpsp06
 def testDependenciesUnFullfilled():
-   partial_path_2 = [1, 2, 4, 5]
-   if checkDependencies(partial_path_2, rcpsp06_get_task_dependencies()):
-       print("Test case 2: Dependencies fulfilled for rcpsp06")
-   else:
-       print("Test case 2: Dependencies not fulfilled for rcpsp06")
-   return 0
+    partial_path_2 = [1, 2, 4, 5]
+    result = checkDependencies(partial_path_2, rcpsp06_get_task_dependencies())
+    resultDependencies(result)
+    return 0
+
+def resultDependencies(result):
+    if result is True:
+        print("Test case 1: Dependencies fulfilled for rcpsp06")
+    else:
+        print("Test case 2: Dependencies not fulfilled for rcpsp06")
 
 """
 RESOURCES
 ---------------------------------------------------------------------------------------------------
 """
+# Test case 1: Partial path with fulfilled resources for rcpsp06
+def testResourcesFulfilled():
+    partial_path_1 = [1, 2, 3]
+    task_duration = get_task_duration()
+    task_resource = get_task_resource()
+    resources = 4
+
+    result = checkResources(partial_path_1, task_duration, task_resource, resources)
+    resultResources(result)
+    return 0
+
+# Test case 1: Partial path with fulfilled resources for rcpsp06
+def testResourcesUnfulfilled():
+    partial_path_2 = [1, 2, 3, 4]  # Assume this path exceeds the available resources
+    task_duration = get_task_duration()
+    task_resource = get_task_resource()
+    resources = 2
+
+    result = checkResources(partial_path_2, task_duration, task_resource, resources)
+    resultResources(result)
+    return 0
+
+def resultResources(result):
+    if result is True:
+        print("Test case 1: Resources fulfilled for rcpsp06")
+    else:
+        print("Test case 2: Resources not fulfilled for rcpsp06")
