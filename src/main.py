731fb22541308374bb4283721297774b9fdcaa72
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-11-24 16:13:14 +0100

    Main divided in four parts: 1. imports and renaming of methods form other classes and packages. 2. method which verifies that methods checkDependencies and checkResources work correctly. 3. method which contains the execution of the taskScheduler, with two "match" that allow to choose the problem and algorithm that are going to be used. 4. the execution itself of the taskScheduler (print of the name, execution of the previous two methods)

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# 1. IMPORTS
 "FOR TESTING:"
 from src.checks.testing_checkings import testDependenciesUnFullfilled
 from src.checks.testing_checkings import testDependenciesFullfilled
@@ -38,6 +39,7 @@ from src.upmevo.evo_exercises import exercise4 as advancedGenetic
 from src.upmsearch.search_exercises import exercise1 as branchAndBound
 from src.upmsearch.search_exercises import exercise2 as aStar
 
+# 2. VERIFICATION OF CHECKDEPENDENCIES AND CHECKRESOURCES
 def checkingAlg():
     print("Testing that the method checkDependencies works correctly, at least for rcpsp06 (for example)")
     testDependenciesFullfilled()
@@ -50,60 +52,56 @@ def checkingAlg():
     testResourcesUnfulfilled()
     print("-----------------------------------------------------------------------------------------------")
 
-    print('\nTask Scheduler')
-
+# 3. TASKSCHEDULER (CHOOSING
 def taskScheduler():
     choiceAlg = -1
-    choiceProb = -1
     while choiceAlg != 0:
         choiceAlg = menuAlgorithm()
-        choiceProb = menuProblems()
-        # First, the date needed is assigned based on the previous choice madr
-        match choiceProb:
-            case 1:
-                tasks = get_tasks_rcpsp06()
-                resources = get_resources_rcpsps06()
-                task_duration = get_task_duration_rcpsp06()
-                task_resource = get_task_resource_rcpsp06()
-                task_dependencies = get_task_dependencies_rcpsp06()
-            case 2:
-                tasks = get_tasks_rcpsp07()
-                resources = get_resources_rcpsp07()
-                task_duration = get_task_duration_rcpsp07()
-                task_resource = get_task_resource_rcpsp07()
-                task_dependencies = get_task_dependencies_rcpsp07()
-            case 3:
-                tasks = get_tasks_rcpsp10()
-                resources = get_resources_rcpsp10()
-                task_duration = get_task_duration_rcpsp10()
-                task_resource = get_task_resource_rcpsp10()
-                task_dependencies = get_task_dependencies_rcpsp10()
-            case 4:
-                tasks = get_tasks_rcpsp30()
-                resources = get_resources_rcpsp30()
-                task_duration = get_task_duration_rcpsp30()
-                task_resource = get_task_resource_rcpsp30()
-                task_dependencies = get_task_dependencies_rcpsp30()
-            case _:
-                print("A problem has occured with the data")
+        if choiceAlg != 0:
+            choiceProb = menuProblems()
+            # First, the date needed is assigned based on the previous choice madr
+            match choiceProb:
+                case 1:
+                    tasks = get_tasks_rcpsp06()
+                    resources = get_resources_rcpsps06()
+                    task_duration = get_task_duration_rcpsp06()
+                    task_resource = get_task_resource_rcpsp06()
+                    task_dependencies = get_task_dependencies_rcpsp06()
+                case 2:
+                    tasks = get_tasks_rcpsp07()
+                    resources = get_resources_rcpsp07()
+                    task_duration = get_task_duration_rcpsp07()
+                    task_resource = get_task_resource_rcpsp07()
+                    task_dependencies = get_task_dependencies_rcpsp07()
+                case 3:
+                    tasks = get_tasks_rcpsp10()
+                    resources = get_resources_rcpsp10()
+                    task_duration = get_task_duration_rcpsp10()
+                    task_resource = get_task_resource_rcpsp10()
+                    task_dependencies = get_task_dependencies_rcpsp10()
+                case 4:
+                    tasks = get_tasks_rcpsp30()
+                    resources = get_resources_rcpsp30()
+                    task_duration = get_task_duration_rcpsp30()
+                    task_resource = get_task_resource_rcpsp30()
+                    task_dependencies = get_task_dependencies_rcpsp30()
+                case _:
+                    print("A problem has occured with the data")
 
-        #Then, the algorithm starts (depending on the previous choice made), with its data already prepared
-        match choiceAlg:
-            case 1:
-                branchAndBound(tasks, resources, task_duration, task_resource, task_dependencies)
-            case 2:
-                aStar(tasks, resources, task_duration, task_resource, task_dependencies)
-            case 3:
-                seed = 0 #HERE, CORREGIR !!
-                simpleGenetic(seed, tasks,resources, task_duration, task_resource, task_dependencies)
-            case 4:
-                seed = 0 #HERE, CORREGIR !!
-                advancedGenetic(seed, tasks, resources, task_duration, task_resource, task_dependencies)
+            # Then, the algorithm starts (depending on the previous choice made), with its data already prepared
+            match choiceAlg:
+                case 1:
+                    branchAndBound(tasks, resources, task_duration, task_resource, task_dependencies)
+                case 2:
+                    aStar(tasks, resources, task_duration, task_resource, task_dependencies)
+                case 3:
+                    seed = 0  # HERE, CORREGIR !!
+                    simpleGenetic(seed, tasks, resources, task_duration, task_resource, task_dependencies)
+                case 4:
+                    seed = 0  # HERE, CORREGIR !!
+                    advancedGenetic(seed, tasks, resources, task_duration, task_resource, task_dependencies)
+    print("Execution completed. Thank you")
 
-
-
-
-
-print("TESTING CHECKS")
+print('\nTask Scheduler')
 checkingAlg()
 taskScheduler()
