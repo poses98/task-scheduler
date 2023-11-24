d81d81a969f522695cb4dc3d76a6cc5c0f924180
Aconi-dot <90859549+Aconi-dot@users.noreply.github.com>
2023-11-24 13:07:43 +0100

    Testing checkDependencies in main

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 
 print('Task Scheduler')
+
+
+"DELETE"
+from src.checks.testing_checkings import testDependenciesUnFullfilled
+from src.checks.testing_checkings import testDependenciesFullfilled
+print("Testing that the method checkDependencies works correctly, at least for rcpsp06 (for example)")
+testDependenciesFullfilled()
+testDependenciesUnFullfilled()
