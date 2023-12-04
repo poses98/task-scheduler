1faf39f6abe3ed4db2f468b611e2dc3d49af6ab6
Irene Herrera <irene.herreralop@alumnos.upm.es>
2023-12-04 21:44:57 +0100

    Algo avanzado el A*

--- src/upmsearch/search_exercises.py
+++ src/upmsearch/search_exercises.py
@@ -1,3 +1,5 @@
+import checks.checkings as checks
+
 
 def exercise1(tasks=0, resources=0, task_duration=[], task_resource=[], task_dependencies=[]):
     """
@@ -24,4 +26,35 @@ def exercise2(tasks=0, resources=0, task_duration=[], task_resource=[], task_dep
     :return: list with the start time of each task in the best solution found, or empty list if no solution was found
     """
     print("Test A*")
+
     return []
+
+def selectDecision(chromosome):
+    best_heuristic = 0
+    selected_task = -1
+    for i in range(len(chromosome)):
+        actual_heuristic = heuristic_function(i, chromosome=chromosome)
+        if actual_heuristic > best_heuristic:
+            selected_task = chromosome[i]
+    return selected_task
+
+def heuristic_function (position, **kwargs):
+    heuristic = 0
+    if checks.checkDependencies(kwargs= ['chromosome'], task_dependencies=kwargs['tasks_dependencies'], task_duration=kwargs['task_duration']):
+        if checks.checkResources(kwargs= ['chromosome'], task_duration=kwargs['task_duration'], task_resource=kwargs['task_resource']):
+            heuristic = heuristic_proposal1(kwargs=['chromosome'])
+    return heuristic
+
+def heuristic_proposal1 (**kwargs):
+    max_duration = -1
+    task_duration = kwargs['task_duration']
+    for current_task in range(len(kwargs= ['chromosome'])):
+        if task_duration[current_task] > max_duration:
+            max_duration = task_duration[current_task]
+    return max_duration
+def astar(chromosome, *args, **kwargs):
+    for i in range(len(chromosome)):
+        chromosome[i] = -1
+    for time in range(len(chromosome)):
+        task = selectDecision(chromosome)
+        chromosome[task] = time
