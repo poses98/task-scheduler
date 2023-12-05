2c070aa6580238da782673617217cad21ea418ab
poses98 <posesand@gmail.com>
2023-12-05 16:15:39 +0100

    fix bug task duration

--- src/checks/testing_checkings.py
+++ src/checks/testing_checkings.py
@@ -1,5 +1,7 @@
 from src.checks.checkings import checkDependencies
+
 from src.upmproblems.rcpsp06 import get_task_dependencies as rcpsp06_get_task_dependencies
+from src.upmproblems.rcpsp06 import get_task_duration as rcpsp06_get_task_duration
 
 from src.checks.checkings import checkResources
 from src.upmproblems.rcpsp06 import get_task_duration, get_task_resource
@@ -8,19 +10,23 @@ from src.upmproblems.rcpsp06 import get_task_duration, get_task_resource
 DEPENDENCIES
 ---------------------------------------------------------------------------------------------------
 """
+
+
 # Test case 1: Partial path with fulfilled dependencies for rcpsp06
 def testDependenciesFullfilled():
-   partial_path_1 = [1, 2, 3, 5]
-   result = checkDependencies(partial_path_1, rcpsp06_get_task_dependencies())
-   resultDependencies(result)
-   return 0
+    partial_path_1 = [1, 2, 3, 5]
+    result = checkDependencies(partial_path_1, rcpsp06_get_task_dependencies(), rcpsp06_get_task_duration())
+    resultDependencies(result)
+    return 0
+
 
 # Test case 2: Partial path with unfulfilled dependencies for rcpsp06
 def testDependenciesUnFullfilled():
     partial_path_2 = [1, 2, 4, 5]
-    result = checkDependencies(partial_path_2, rcpsp06_get_task_dependencies())
+    result = checkDependencies(partial_path_2, rcpsp06_get_task_dependencies(), rcpsp06_get_task_duration())
     resultDependencies(result)
     return 0
+
 
 def resultDependencies(result):
     if result is True:
@@ -28,10 +34,13 @@ def resultDependencies(result):
     else:
         print("Test case 2: Dependencies not fulfilled for rcpsp06")
 
+
 """
 RESOURCES
 ---------------------------------------------------------------------------------------------------
 """
+
+
 # Test case 1: Partial path with fulfilled resources for rcpsp06
 def testResourcesFulfilled():
     partial_path_1 = [1, 2, 3]
@@ -43,6 +52,7 @@ def testResourcesFulfilled():
     resultResources(result)
     return 0
 
+
 # Test case 1: Partial path with fulfilled resources for rcpsp06
 def testResourcesUnfulfilled():
     partial_path_2 = [1, 2, 3, 4]  # Assume this path exceeds the available resources
@@ -53,6 +63,7 @@ def testResourcesUnfulfilled():
     result = checkResources(partial_path_2, task_duration, task_resource, resources)
     resultResources(result)
     return 0
+
 
 def resultResources(result):
     if result is True:
